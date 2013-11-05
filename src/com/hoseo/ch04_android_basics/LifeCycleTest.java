7031779e5d8d102b88bf9e695ad69d22cb2255a6
macmini <macmini@macminiui-Mac-mini-2.local>
2013-11-05 16:38:50 +0900

    프로젝트 시작.

---
+++ src/com/hoseo/ch04_android_basics/LifeCycleTest.java
@@ -0,0 +1,46 @@
+package com.hoseo.ch04_android_basics;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class LifeCycleTest extends Activity {
+	StringBuilder builder = new StringBuilder();
+	TextView textView;
+	
+	private void log(String text) {
+		Log.d("LifeCycleTest", text);
+		builder.append(text);
+		builder.append('\n');
+		textView.setText(builder.toString());
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		textView = new TextView(this);
+		textView.setText(builder.toString());
+		setContentView(textView);
+		log("created");
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		log("paused");
+		
+		if (isFinishing()) {
+			log("finishing");
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+	
+}
